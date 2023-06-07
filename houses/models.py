d17b8f4a9fe66a5f5a57afbf914b5416ce39c987
ehghksvjscl <ehghkswodnjs@naver.com>
2023-06-08 02:41:50 +0900

    [model] class 오타 수종

--- houses/models.py
+++ houses/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 
 
-class Houes(models.Model):
+class House(models.Model):
     name = models.CharField(max_length=140)
     price = models.PositiveBigIntegerField()
     description = models.TextField()
