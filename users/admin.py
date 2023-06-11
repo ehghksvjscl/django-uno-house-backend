0a18a5526a685011efbb9e1f8c4c7fc60ddd9ec3
ehghksvjscl <ehghkswodnjs@naver.com>
2023-06-11 16:32:32 +0900

    [model] users mode 추가 및 마이그레이션 초기화

---
+++ users/admin.py
@@ -0,0 +1,8 @@
+from django.contrib import admin
+from users.models import User
+from django.contrib.auth.admin import UserAdmin
+
+
+@admin.register(User)
+class CustomUserAdmin(UserAdmin):
+    pass
