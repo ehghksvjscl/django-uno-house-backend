2f890ae49c246c650058712ec1a687933115fd53
ehghksvjscl <ehghkswodnjs@naver.com>
2023-07-25 07:35:22 +0900

    [ADMIN] 선택된 room의 price를 0으로 만드는 action 추가

--- rooms/admin.py
+++ rooms/admin.py
@@ -2,11 +2,20 @@ from django.contrib import admin
 from . import models
 
 
+@admin.action(description="금액을 0으로 변경")
+def set_zero(model_admin, request, rooms):
+    for room in rooms:
+        room.price = 0
+        room.save()
+
+
 @admin.register(models.Room)
 class RoomAdmin(admin.ModelAdmin):
+    actions = [set_zero]
     list_display = (
         "id",
         "name",
+        "price",
         "country",
         "city",
         "host",
