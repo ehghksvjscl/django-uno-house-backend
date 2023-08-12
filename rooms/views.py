3c74087f6b3e6d85bd2d5ef3f78c774d2f76a667
ehghksvjscl <ehghkswodnjs@naver.com>
2023-08-13 01:52:23 +0900

    [API] rooms -> amenity API 생성  - GET 메소드로 전체 amenities 조회  - POST 메소드로 amenities 생성  - Urls.py 추가 - amenity Serializer 로직 구현

--- rooms/views.py
+++ rooms/views.py
@@ -1,3 +1,36 @@
-from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework.status import HTTP_400_BAD_REQUEST
 
-# Create your views here.
+from .serializers import AmenitySerializer
+from .models import Amenity
+
+
+class AmenityApiView(APIView):
+    def get_object(self):
+        return Amenity.objects.all()
+
+    def get(self, _):
+        qs = self.get_object()
+        serializer = AmenitySerializer(qs, many=True)
+        return Response(serializer.data)
+
+    def post(self, request):
+        data = request.data
+        serializer = AmenitySerializer(data=data)
+        if not serializer.is_valid():
+            return Response(serializer.errors, status=HTTP_400_BAD_REQUEST)
+
+        serializer = serializer.save()
+        return Response(AmenitySerializer(serializer).data)
+
+
+class AmenityDetailApiView(APIView):
+    def get(self, request):
+        pass
+
+    def put(self, request):
+        pass
+
+    def delete(self, request):
+        pass
