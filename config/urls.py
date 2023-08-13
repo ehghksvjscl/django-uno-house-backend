3d117ab611dec1c867f0b9c9e3a362cd6cf57c97
ehghksvjscl <ehghkswodnjs@naver.com>
2023-08-13 16:52:43 +0900

    [API] experiences -> perks API 생성  - Urls.py 추가  - perk Serializer 로직 구현  - GET 메소드로 전체 perks 조회  - POST 메소드로 perk 생성  - 단일 perk를 볼 수 있는 GET 구현  - 단일 perk를 수정할 수 있는 POST 구현  - 단일 perk를 삭제할 수 있는 DELETE 구현

--- config/urls.py
+++ config/urls.py
@@ -6,6 +6,7 @@ urlpatterns = [
     path("admin/", admin.site.urls),
     path("api-auth/", include("rest_framework.urls")),
     path("api/v1/rooms/", include("rooms.urls")),
+    path("api/v1/experiences/", include("experiences.urls")),
 ]
 
 
--- experiences/views.py
+++ experiences/views.py
@@ -1,3 +1,56 @@
-from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework.status import (
+    HTTP_400_BAD_REQUEST,
+    HTTP_204_NO_CONTENT,
+    HTTP_201_CREATED,
+)
+from rest_framework.exceptions import NotFound
 
-# Create your views here.
+from .serializers import PerkSerializer
+from .models import Perk
+
+
+class PerkApiView(APIView):
+    def get_object(self):
+        return Perk.objects.all()
+
+    def get(self, _):
+        qs = self.get_object()
+        serializer = PerkSerializer(qs, many=True)
+        return Response(serializer.data)
+
+    def post(self, request):
+        data = request.data
+        serializer = PerkSerializer(data=data)
+        if not serializer.is_valid():
+            return Response(serializer.errors, status=HTTP_400_BAD_REQUEST)
+
+        serializer = serializer.save()
+        return Response(PerkSerializer(serializer).data, status=HTTP_201_CREATED)
+
+
+class PerkDetailApiView(APIView):
+    def get_object(self, pk):
+        try:
+            return Perk.objects.get(pk=pk)
+        except Perk.DoesNotExist:
+            raise NotFound
+
+    def get(self, _, pk):
+        qs = self.get_object(pk=pk)
+        return Response(PerkSerializer(qs).data)
+
+    def put(self, request, pk):
+        qs = self.get_object(pk=pk)
+        serializer = PerkSerializer(qs, data=request.data, partial=True)
+        if not serializer.is_valid():
+            return Response(serializer.errors, status=HTTP_400_BAD_REQUEST)
+
+        serializer = serializer.save()
+        return Response(PerkSerializer(serializer).data)
+
+    def delete(self, _, pk):
+        qs = self.get_object(pk=pk)
+        qs.delete()
+        return Response(status=HTTP_204_NO_CONTENT)
